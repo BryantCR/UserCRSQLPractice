2606ff4c59ff0b5a9e1339697944d542bebd8637
BryantCR <bryancvargas20@gmail.com>
2021-09-29 15:56:31 -0600

    ok

--- server.py
+++ server.py
@@ -5,12 +5,20 @@ from User import User
 app = Flask( __name__ )
 app.secret_key = "secret"
 
+@app.route( "/", methods=['GET'] )
+def displayLogin():
+    return render_template( "Read.html")
+
+
 @app.route( "/users", methods=['GET'] )
 def displayAllUsers():
-    loginError = ""
-    if 'loginError' in session:
-        loginError = session['loginError']
-    return render_template( "index.html", loginError=loginError )
+    # if 'id' in session:
+    #     id = session['id']
+    #     currentUserData = users[ id ]
+    #     print( currentUserData )
+        return render_template( "Read.html", users=currentUserData )
+    # else:
+    #     return "Error Loading The Page"
 
 
 @app.route( "/users/new", methods=['POST'] )
@@ -26,7 +34,7 @@ def validateCredentials():
                 session.pop( 'loginError' )
             return redirect( '/home' )
     session['loginError'] = "Wrong credentials provided."
-    return redirect( '/login' )
+    return redirect( '/users' )
 
 
 
@@ -36,5 +44,27 @@ if __name__ == "__main__":
     app.run( debug = True )
 
 
+# @app.route('/')
+# def index():
+#     return redirect('/users')
 
+
+# @app.route('/users')
+# def users():
+#     return render_template("users.html",users=User.get_all())
+
+
+# @app.route('/user/new')
+# def new():
+#     return render_template("new_user.html")
+
+# @app.route('/user/create',methods=['POST'])
+# def create():
+#     print(request.form)
+#     User.save(request.form)
+#     return redirect('/users')
+
+
+# if __name__=="__main__":
+#     app.run(debug=True)
 
